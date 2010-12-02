ef482c449ee98b7d4ffdde23006c788b5b69328f
Ashay Humane <ashay.humane@gmail.com>
2010-12-01 19:12:29 -0800

    Linux package tracker. Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lpt::Application.config.secret_token = '4aa42a7b4b227dd0a6bacc3b0127c4add9e4fe344e87edac91947ff6c54fe062bb853a66186d721726355f7b0d06ef8f28d82125485c2b6e4653dae84082470e'
