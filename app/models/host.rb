3d695bc40ebf02529d2966f15670541735a2609e
Maulin Pathare <mpathare@gmail.com>
2010-12-02 07:13:57 -0800

    Reminder to use :select instead of :uniq for large collections.

--- app/models/host.rb
+++ app/models/host.rb
@@ -3,6 +3,11 @@ class Host < ActiveRecord::Base
   # bug in dependent => destory, should be fixed in 2.3.6 #2251
   after_destroy {|record| Installation.delete_all("host_id = #{record.id}")}
   has_many :packages, :through => :installations, :uniq => true
+
+  #The :uniq option removes duplicates in Ruby code, not in the database query. 
+  #If you have a large number of duplicates, it might be better to use the :select option to tell the 
+  #database to remove duplicates using the DISTINCT keyword :select => "DISTINCT packages.*"
+
   has_one :os, :through => :installations
   validates_uniqueness_of :name
 
