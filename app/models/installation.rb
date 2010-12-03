f098d5ddc2335102b793efdc3cc24b452e51ad48
Ashay Humane <ashay.humane@gmail.com>
2010-12-03 14:20:21 -0800

    Installation.rb check for existing method

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -15,14 +15,19 @@ class Installation < ActiveRecord::Base
     puts "#{host.id} #{os.id} #{pkgs[0]}"
     pkgs.each do |pkg|
       pkg, version, release, arch, installed_on = pkg.split("===").map{|s| s.chomp.strip}
-      unless install=Installation.first(:joins => [:package, :version, :release, :arch],
-                :conditions => {:host_id => host.id, 
-                                :os_id => os.id, 
-                                :packages => {:name => pkg},
-                                :versions => {:value => version}, 
-                                :releases => {:value => release}, 
-                                :arches => {:name => arch}})
-
+      #unless install=Installation.first(:joins => [:package, :version, :release, :arch],
+      #          :conditions => {:host_id => host.id, 
+      #                          :os_id => os.id, 
+      #                          :packages => {:name => pkg},
+      #                          :versions => {:value => version}, 
+      #                          :releases => {:value => release}, 
+      #                          :arches => {:name => arch}})
+      unless install=Installation.joins(:package, :version, :release, :arch).where(:host_id => 2,
+                                                                            :os_id => 1,
+                                                                            :packages => {:name => "libdmapsharing"},
+                                                                            :versions => {:value => "1.9.0.21"},
+                                                                            :releases => {:value => "1.fc14"},
+                                                                            :arches => {:name => "i686"} ).first
         new_pkgs += pkg.to_a
         p_id = Package.find_or_create_by_name(pkg).id
         v_id = Version.find_or_create_by_value(version).id
