922c211531f311daaea5317b443b2779394aee36
Ashay Humane <ashay.humane@gmail.com>
2010-12-25 21:53:51 -0800

    Include resque-status

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,3 +2,7 @@ Dir["#{Rails.root}/lib/jobs/*.rb"].each { |file| require file }
 
 config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
 Resque.redis = Redis.new(:host => config['host'],:port => config['port'])
+
+# resque-status stuff:
+require 'resque/job_with_status'
+Resque::Status.expire_in = (24 * 60 * 60) # 24hrs in seconds
