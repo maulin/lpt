f1fd49db524b6917884d1bd27104caf15d9c739b
Maulin Pathare <mPathare@gmail.com>
2011-01-29 18:47:45 -0500

    added sha to repo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110128195510) do
+ActiveRecord::Schema.define(:version => 20110129234709) do
 
   create_table "arches", :force => true do |t|
     t.string   "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(:version => 20110128195510) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "repo_type"
+    t.string   "sha"
   end
 
   create_table "sources", :force => true do |t|
